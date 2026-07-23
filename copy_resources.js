0abe1ffe2512ddcc2ecf2ec4d548bbcaab27a987
兮. <1269838578@qq.com>
2026-07-23 17:36:40 +0800

    build(vite): 配置rollup输出选项以指定入口文件名
    - 在vite配置中添加rollupOptions以自定义打包输出
    - 设置entryFileNames为'live2dplugin.js'确保输出文件名一致
    - 保持现有的开发模式源码映射功能
    - 维持原有的构建目录和清空设置选项

--- copy_resources.js
+++ copy_resources.js
@@ -7,12 +7,28 @@
 
 "use strict";
 const fs = require('fs');
+
+// 尝试多种路径，以适应不同的构建环境
+function findResourcePath(possiblePaths) {
+  for (const path of possiblePaths) {
+    if (fs.existsSync(path)) {
+      return path;
+    }
+  }
+  throw new Error(`Could not find resource directory. Tried paths: ${possiblePaths.join(', ')}`);
+}
+
+// 定义可能的资源路径
+const corePath = findResourcePath(['../../../Core', '../public/Core', './public/Core', '../../Core']);
+const resourcesPath = findResourcePath(['../../Resources', '../public/Resources', './public/Resources', '../Resources']);
+const shadersPath = findResourcePath(['../../../Framework/Shaders', '../public/Framework/Shaders', './public/Framework/Shaders', '../../Framework/Shaders']);
+const frameworkSrcPath = findResourcePath(['../../../Framework/src', '../public/Framework/src', './public/Framework/src', '../../Framework/src']);
+
 const publicResources = [
-  {src: '../../../Core', dst: './public/Core'},
-  {src: '../../Resources', dst: './public/Resources'},
-  {src: '../../../Framework/Shaders', dst: './public/Framework/Shaders'},
-  {src: '../../../Framework/src', dst: './public/Framework/src'},
-  {src: '../../../Framework/src', dst: './public/Framework/src'}, // 着色器文件也需要复制到 dist/Framework/Shaders
+  {src: corePath, dst: './public/Core'},
+  {src: resourcesPath, dst: './public/Resources'},
+  {src: shadersPath, dst: './public/Framework/Shaders'},
+  {src: frameworkSrcPath, dst: './public/Framework/src'},
 ];
 
 publicResources.forEach((e)=>{if (fs.existsSync(e.dst)) fs.rmSync(e.dst, { recursive: true })});
